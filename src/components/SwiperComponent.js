d2315f943bbdfd18eb307fbb87e050bdeb60bb3f
이한승 <itstrap1016@gmail.com>
2025-05-01 14:47:26 +0900

    Swiper 수정

--- src/components/SwiperComponent.js
+++ src/components/SwiperComponent.js
@@ -1,3 +1,9 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 const SwiperComponet = () => {
   return `
 
@@ -31,6 +37,7 @@ export const setSwiper = () => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    modules: [Navigation, Pagination],
   });
 };
 
