dd873052df2767571554c38256f7fdea05cb5ae2
이한승 <itstrap1016@gmail.com>
2025-05-05 16:00:14 +0900

    회원가입 스타일링

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -14,61 +14,72 @@ const SignUpForm = () => {
       <form class="signup-form">
         <div class="fieldset-wrapper">
             <fieldset class="id-input-set">
-            <label for="user-id">아이디</label>
-            <div class="input-button">
-                <input id="user-id" class="signup-input" type="text"/>
-                <button class="primary-btn" type="button">
-                중복확인
-                </button>
-            </div>
-            ${idError ? ErrorMessage(idErrorMessage) : ""}
+              <label for="user-id">아이디</label>
+              <div class="input-button">
+                  <input id="user-id" class="signup-input" type="text"/>
+                  <button class="primary-btn" type="button">
+                  중복확인
+                  </button>
+              </div>
+              ${idError ? ErrorMessage(idErrorMessage) : ""}
             </fieldset>
             <fieldset class="password-input-set">
-            <label for="password">비밀번호</label>
-            <div class="input-check">
-                <input id="password" class="signup-input" type="password" />
-                <input class="password-check-btn" type="checkbox" />
-            </div>
-            ${passwordError ? ErrorMessage(passwordErrorMessage) : ""}
+              <label for="password">비밀번호</label>
+              <div class="input-check">
+                  <input id="password" class="signup-input" type="password" />
+                  <input class="password-check-btn" type="checkbox" />
+                  <div class="custom-check-btn"></div>
+              </div>
+              ${passwordError ? ErrorMessage(passwordErrorMessage) : ""}
             </fieldset>
             <fieldset class="password-check-input-set">
-            <label for="password-check">비밀번호 재확인</label>
-            <div class="input-check">
-                <input id="password-check" class="signup-input" type="password" />
-                <input class="password-check-btn" type="checkbox" />
-            </div>
-            ${passwordCheckError ? ErrorMessage(passwordCheckErrorMessage) : ""}
+              <label for="password-check">비밀번호 재확인</label>
+              <div class="input-check">
+                  <input id="password-check" class="signup-input" type="password" />
+                  <input class="password-check-btn" type="checkbox" />
+                  <div class="custom-check-btn"></div>
+              </div>
+              ${
+                passwordCheckError
+                  ? ErrorMessage(passwordCheckErrorMessage)
+                  : ""
+              }
             </fieldset>
             <fieldset class="name-input-set">
-            <label for="name">이름</label>
-            <input id="name" class="signup-input" type="text" />
+              <label for="name">이름</label>
+              <input id="name" class="signup-input" type="text" />
             </fieldset>
-            <fieldset>
-            <label for="phone-number-select">휴대폰번호</label>
-            <div class="inputs-wrapper">
-                <select id="phone-number-select">
-                <option value="010">010</option>
-                <option value="011">011</option>
-                <option value="012">012</option>
-                <option value="013">013</option>
-                <option value="014">014</option>
-                <option value="015">015</option>
-                <option value="016">016</option>
-                <option value="017">017</option>
-                <option value="018">018</option>
-                <option value="019">019</option>
-                </select>
-                <input type="number" />
-                <input type="number" />
-            </div>
-            ${personalInfoError ? ErrorMessage(personalInfoErrorMessage) : ""}
+            <fieldset class="phone-number-set">
+              <label for="phone-number-select">휴대폰번호</label>
+              <div class="inputs-wrapper">
+                  <select id="phone-number-select">
+                    <option value="010">010</option>
+                    <option value="011">011</option>
+                    <option value="012">012</option>
+                    <option value="013">013</option>
+                    <option value="014">014</option>
+                    <option value="015">015</option>
+                    <option value="016">016</option>
+                    <option value="017">017</option>
+                    <option value="018">018</option>
+                    <option value="019">019</option>
+                  </select>
+                  <input type="number" class="signup-input" />
+                  <input type="number" class="signup-input" />
+              </div>
+              ${personalInfoError ? ErrorMessage(personalInfoErrorMessage) : ""}
             </fieldset>
         </div>
         <div class="terms-agreement">
-          <input type="checkbox" />
-          <p>호두샵의 <span>이용약관</span> 및 <span>개인정보처리방침</span>에 대한 내용을 확인하였고 동의합니다.</p>
+          <div class="checkbox-wrapper">
+            <input type="checkbox" />
+            <div class="custom-checkbox"></div>
+          </div>
+          <p>호두샵의 <a href="#">이용약관</a> 및 <a href="#">개인정보처리방침</a>에 대한 내용을 확인하였고 동의합니다.</p>
         </div>
-        <button type="submit" disabled>가입하기</button>
+        <div class="btn-wrapper">
+          <button class="primary-btn signup-btn" type="submit" disabled>가입하기</button>
+        </div>
       </form>
     `;
 };
