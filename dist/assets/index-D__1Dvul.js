e7aa77d3422ca40ae7f90bf5a6dc058dae982bf2
이한승 <itstrap1016@gmail.com>
2025-05-10 15:14:52 +0900

    dist update

--- dist/assets/index-D__1Dvul.js
+++ dist/assets/index-D__1Dvul.js
@@ -37,7 +37,6 @@
         <section class="logo-search">
           <h1 class="top-logo">
             <a href="/" aria-label="홈페이지로 이동하는 링크">
-              <img src="/src/assets/img/logo-hodu.png" alt="호두 로고" />
             </a>
           </h1>
           <form action="" class="search-form">
@@ -156,7 +155,6 @@
   `,s=new MutationObserver(()=>{Qe(),Hs(),s.disconnect()});return s.observe(document.getElementById("app"),{childList:!0}),e},ot=()=>`
     <h1 class="form-logo">
         <a href="/" aria-label="홈페이지로 이동하는 링크">
-            <img src="/src/assets/img/logo-hodu.png" alt="호두 로고" />
         </a>
     </h1>
     `,W=(t="에러 메세지")=>`
