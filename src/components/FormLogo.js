c9f19152dcb6c5dfbd38f900d4c802be37aa52eb
이한승 <itstrap1016@gmail.com>
2025-05-10 15:13:54 +0900

    로고 이미지 스타일 변경

--- src/components/FormLogo.js
+++ src/components/FormLogo.js
@@ -2,7 +2,6 @@ const FormLogo = () => {
   return `
     <h1 class="form-logo">
         <a href="/" aria-label="홈페이지로 이동하는 링크">
-            <img src="/src/assets/img/logo-hodu.png" alt="호두 로고" />
         </a>
     </h1>
     `;
--- src/components/Header.js
+++ src/components/Header.js
@@ -65,7 +65,6 @@ const Header = () => {
         <section class="logo-search">
           <h1 class="top-logo">
             <a href="/" aria-label="홈페이지로 이동하는 링크">
-              <img src="/src/assets/img/logo-hodu.png" alt="호두 로고" />
             </a>
           </h1>
           <form action="" class="search-form">
